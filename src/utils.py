f8f63402672cb667bf07cfa76112be4c4b36e5e8
Benton Tripp <bentontripp@gmail.com>
2022-05-27 07:16:50 -0400

    append years util in groupby

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,26 @@
 from dateutil.relativedelta import relativedelta
 import random
+import pandas as pd
+import numpy as np
 
-def append_years(data, years=3):
-    start_date = max(data.index) + relativedelta(months=1)
+def append_years(data, years=5):
+    start_date = max(data.date) + relativedelta(months=1)
     new_end_date = start_date + relativedelta(months=years * 12 - 1)
-    idx = pd.date_range(start_date, new_end_date, freq='MS')
+    dates = pd.date_range(start_date, new_end_date, freq='MS')
     new_data = pd.DataFrame(
-        columns=data.columns,
-        data = np.round(data.groupby(data.index.month).mean().values * random.uniform(1, 1.25), 1)
+        {
+            'date' : dates,
+            'category' : data.category.unique()[0],
+            'subcategory' : data.subcategory.unique()[0]
+        }
         )
+    value = np.round(data.groupby(data.date.dt.month).value.mean().values * random.uniform(.5, 2) + random.uniform(0, 2), 1)
     if years > 1:
         for i in range(years - 1):
-            new_data = pd.concat([
-                new_data, 
-                pd.DataFrame(
-                    columns=data.columns,
-                    data = np.round(data.groupby(data.index.month).mean().values * random.uniform(1, 1.25), 1)
-            )], axis=0)
-    new_data.index = idx
-    return new_data
+            value = np.concatenate((
+                value,                
+                np.round(data.groupby(data.date.dt.month).value.mean().values * random.uniform(.5, 2) + random.uniform(0, 2), 1)
+            ), axis=0)
+    new_data['value'] = value
+    data = pd.concat([data, new_data], axis=0)
+    return data.reset_index(drop=True)
